692ad61520cc2365a0e51ef18fd766ffbf1b482f
oliverwhit <66801422+oliverwhit@users.noreply.github.com>
2021-11-05 10:46:47 -0400

    Update 1a-Mobile_Commons_vs_SF.sql

--- 1a-Mobile_Commons_vs_SF.sql
+++ 1a-Mobile_Commons_vs_SF.sql
@@ -41,7 +41,7 @@ FROM sfstep3;
 --Mobile Commons
 CREATE TEMPORARY TABLE mcstep1 AS 
 SELECT
-  right(phone_number, 10) as phone, 
+	right(phone_number, 10) as phone, 
     opted_out_at
 FROM sc_mobilecommons_staging.profiles
 WHERE phone_number IS NOT NULL;
@@ -69,9 +69,41 @@ SELECT
   CASE WHEN opted_out > 0 THEN 'Opt-Out' ELSE 'Opt-In' END AS mc_sms_status
 FROM mcstep3;
 
--- JOIN
-SELECT a.phone, a.mc_sms_status, b.sf_sms_status 
+
+--Hustle
+CREATE TEMPORARY TABLE hstep1 AS 
+SELECT
+	right(phone_number, 10) as phone, 
+    global_opted_out
+FROM sc_analytics.hustle_leads
+WHERE phone_number IS NOT NULL;
+
+CREATE TEMPORARY TABLE hstep2 AS 
+SELECT 
+    phone, 
+    CASE WHEN global_opted_out = 't' THEN '1' ELSE '0' END AS opted_out,
+    CASE WHEN global_opted_out = 'f' THEN '1' ELSE '0' END AS opted_in
+FROM hstep1;
+
+CREATE TEMPORARY TABLE hstep3 AS 
+SELECT 
+  phone, 
+  sum(opted_out) AS opted_out, 
+  sum(opted_in) AS opted_in,
+  count(*) AS total_instances
+FROM hstep2
+GROUP BY 1
+ORDER BY 4 DESC;
+
+CREATE TEMPORARY TABLE hstep4 AS 
+SELECT 
+  phone, 
+  CASE WHEN opted_out > 0 THEN 'Opt-Out' ELSE 'Opt-In' END AS hustle_sms_status
+FROM hstep3;
+
+-- Join
+SELECT a.phone, a.mc_sms_status, b.sf_sms_status
 FROM mcstep4 a
-LEFT JOIN sfstep4 b
+JOIN sfstep4 b
 ON a.phone = b.phone
 WHERE b.sf_sms_status <> a.mc_sms_status;
