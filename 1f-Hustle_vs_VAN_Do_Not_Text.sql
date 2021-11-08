c16e332a19dae479008f95a6cbf5d66f17f7fcd5
oliverwhit <66801422+oliverwhit@users.noreply.github.com>
2021-11-08 14:08:48 -0500

    Update 1f-Hustle_vs_VAN_Do_Not_Text.sql

--- 1f-Hustle_vs_VAN_Do_Not_Text.sql
+++ 1f-Hustle_vs_VAN_Do_Not_Text.sql
@@ -113,33 +113,35 @@ FROM vanstep3;
 --Hustle
 CREATE TEMPORARY TABLE hstep1 AS 
 SELECT
-	right(phone_number, 10) as phone, 
-    global_opted_out
+    right(phone_number, 10) as phone, 
+    global_opted_out, 
+    updated_at,
+    COUNT(*)
 FROM sc_analytics.hustle_leads
-WHERE phone_number IS NOT NULL;
+WHERE phone_number IS NOT NULL
+GROUP BY 1,2,3;
 
 CREATE TEMPORARY TABLE hstep2 AS 
 SELECT 
-    phone, 
-    CASE WHEN global_opted_out = 't' THEN '1' ELSE '0' END AS opted_out,
-    CASE WHEN global_opted_out = 'f' THEN '1' ELSE '0' END AS opted_in
-FROM hstep1;
+    phone, updated_at,
+    CASE WHEN global_opted_out = 't' THEN 'Opt-Out' ELSE 'Opt-In' END AS hustle_sms_status
+FROM hstep1
+;
 
-CREATE TEMPORARY TABLE hstep3 AS 
+CREATE TEMPORARY TABLE hstep3 AS
 SELECT 
-  phone, 
-  sum(opted_out) AS opted_out, 
-  sum(opted_in) AS opted_in,
-  count(*) AS total_instances
-FROM hstep2
-GROUP BY 1
-ORDER BY 4 DESC;
+  phone,
+  MAX(updated_at) AS latest_update
+FROM hstep1
+GROUP BY 1;
 
-CREATE TEMPORARY TABLE hstep4 AS 
-SELECT 
-  phone, 
-  CASE WHEN opted_out > 0 THEN 'Opt-Out' ELSE 'Opt-In' END AS hustle_sms_status
-FROM hstep3;
+CREATE TEMPORARY TABLE hstep4 AS
+SELECT a.phone, a.latest_update, b.hustle_sms_status
+FROM hstep3 a
+LEFT JOIN hstep2 b
+  ON a.phone = b.phone
+  AND a.latest_update = b.updated_at;
+
 
 -- JOIN
 SELECT a.phone, a.van_sms_status, b.hustle_sms_status
